2bd0b738d87aa8fdc2ae4032e6d4c14192ff0467
GOYEJIN <yaejingo@gmail.com>
2023-05-12 12:31:20 +0900

    project set

---
+++ src/main/java/com/sparta/kreamclone_backend/KreamCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kreamclone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreamCloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KreamCloneBackendApplication.class, args);
+    }
+
+}
